e866ff4c92ac1572005aa2240719a02aa788d47f
CeebTV <cyril.beeckman@gmail.com>
2023-08-05 17:51:09 +0200

    Update shared.lua

--- shared.lua
+++ shared.lua
@@ -48,8 +48,9 @@ if GetConvar('voice_useNativeAudio', 'false') == 'true' then
     -- native audio distance seems to be larger then regular gta units
     Cfg.voiceModes = {
         { 1.5, "Whisper" }, -- Whisper speech distance in gta distance units
-        { 3.0, "Normal" },  -- Normal speech distance in gta distance units
-        { 6.0, "Shouting" } -- Shout speech distance in gta distance units
+        { 5.0, "Normal" }, -- Normal speech distance in gta distance units
+        { 15.0, "Shouting" }, -- Shout speech distance in gta distance units
+        { 25.0, "MAX" }
     }
 else
     Cfg.voiceModes = {
