a1cfb3640c84a1061a37bdbf115ccfcaf52a6f9d
Cyril Beeckman <cyril.beeckman@gmail.com>
2023-11-01 18:40:04 +0100

    switch indent to tabs

--- client/init/submix.lua
+++ client/init/submix.lua
@@ -1,81 +1,81 @@
 ConfigSubmixes = {
-    micro = {
-        filters = {
-            freq_low    = 50.0,
-            freq_hi     = 10000.0,
-            rm_mod_freq = 300.0,
-            rm_mix      = 0.05,
-            fudge       = 1.0,
-            o_freq_lo   = 50.0,
-            o_freq_hi   = 6000.0,
-        }
-    },
-    megaphone = {
-        filters = {
-            freq_low    = 10.0,
-            freq_hi     = 10000.0,
-            rm_mod_freq = 300.0,
-            rm_mix      = 0.2,
-            fudge       = 0.0,
-            o_freq_lo   = 200.0,
-            o_freq_hi   = 5000.0,
-        }
-    },
-    muffled = {
-        filters = {
-            freq_low  = 0.0,
-            freq_hi   = 850.0,
-            rm_mix    = 0.00,
-            o_freq_lo = 0.0,
-            o_freq_hi = 850.0,
-        }
-    },
-    radio_default = {
-        filters = {
-            freq_low    = 100.0,
-            freq_hi     = 5000.0,
-            rm_mod_freq = 300.0,
-            rm_mix      = 0.1,
-            fudge       = 4.0,
-            o_freq_lo   = 300.0,
-            o_freq_hi   = 5000.0,
-        }
-    },
-    radio_medium_distance = {
-        filters = {
-            freq_low    = 100.0,
-            freq_hi     = 5000.0,
-            rm_mod_freq = 300.0,
-            rm_mix      = 0.5,
-            fudge       = 10.0,
-            o_freq_lo   = 300.0,
-            o_freq_hi   = 5000.0,
-        }
-    },
-    radio_far_distance = {
-        filters = {
-            freq_low    = 100.0,
-            freq_hi     = 5000.0,
-            rm_mod_freq = 300.0,
-            rm_mix      = 0.8,
-            fudge       = 16.0,
-            o_freq_lo   = 300.0,
-            o_freq_hi   = 5000.0,
-        }
-    },
+	micro = {
+		filters = {
+			freq_low    = 50.0,
+			freq_hi     = 10000.0,
+			rm_mod_freq = 300.0,
+			rm_mix      = 0.05,
+			fudge       = 1.0,
+			o_freq_lo   = 50.0,
+			o_freq_hi   = 6000.0,
+		}
+	},
+	megaphone = {
+		filters = {
+			freq_low    = 10.0,
+			freq_hi     = 10000.0,
+			rm_mod_freq = 300.0,
+			rm_mix      = 0.2,
+			fudge       = 0.0,
+			o_freq_lo   = 200.0,
+			o_freq_hi   = 5000.0,
+		}
+	},
+	muffled = {
+		filters = {
+			freq_low  = 0.0,
+			freq_hi   = 850.0,
+			rm_mix    = 0.00,
+			o_freq_lo = 0.0,
+			o_freq_hi = 850.0,
+		}
+	},
+	radio_default = {
+		filters = {
+			freq_low    = 100.0,
+			freq_hi     = 5000.0,
+			rm_mod_freq = 300.0,
+			rm_mix      = 0.1,
+			fudge       = 4.0,
+			o_freq_lo   = 300.0,
+			o_freq_hi   = 5000.0,
+		}
+	},
+	radio_medium_distance = {
+		filters = {
+			freq_low    = 100.0,
+			freq_hi     = 5000.0,
+			rm_mod_freq = 300.0,
+			rm_mix      = 0.5,
+			fudge       = 10.0,
+			o_freq_lo   = 300.0,
+			o_freq_hi   = 5000.0,
+		}
+	},
+	radio_far_distance = {
+		filters = {
+			freq_low    = 100.0,
+			freq_hi     = 5000.0,
+			rm_mod_freq = 300.0,
+			rm_mix      = 0.8,
+			fudge       = 16.0,
+			o_freq_lo   = 300.0,
+			o_freq_hi   = 5000.0,
+		}
+	},
 }
 
 CreateThread(function()
-    for submixName, submix in pairs(ConfigSubmixes) do
-        local submixId = CreateAudioSubmix(submixName)
-        SetAudioSubmixEffectRadioFx(submixId, 0)
-        SetAudioSubmixEffectParamInt(submixId, 0, `default`, 1)
-        for key, value in pairs(submix.filters) do
-            SetAudioSubmixEffectParamFloat(submixId, 0, GetHashKey(key), value)
-        end
-        AddAudioSubmixOutput(submixId, 0)
-        submixIndicies[submixName] = submixId
-    end
+	for submixName, submix in pairs(ConfigSubmixes) do
+		local submixId = CreateAudioSubmix(submixName)
+		SetAudioSubmixEffectRadioFx(submixId, 0)
+		SetAudioSubmixEffectParamInt(submixId, 0, `default`, 1)
+		for key, value in pairs(submix.filters) do
+			SetAudioSubmixEffectParamFloat(submixId, 0, GetHashKey(key), value)
+		end
+		AddAudioSubmixOutput(submixId, 0)
+		submixIndicies[submixName] = submixId
+	end
 end)
 
 AddStateBagChangeHandler("submix", "", function(bagName, _, value)
